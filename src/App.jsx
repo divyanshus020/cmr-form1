46572e4220c1f063f9358968d2044983c6944030
divyanshu <divyanshu>
2024-06-28 09:57:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import AuthPage from './component/AuthPage';
+
+function App() {
+  return (
+    <div className="App">
+      <AuthPage />
+    </div>
+  );
+}
+
+export default App;
